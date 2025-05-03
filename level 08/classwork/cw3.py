0a7257866876bbe6c9c734c33312cc0c2f6b8385
gabi2605k <gabriel.qarqashadze@icloud.com>
2025-05-03 12:15:04 +0400

    commit

---
+++ level 08/classwork/cw3.py
@@ -0,0 +1,4 @@
+# 3)შექმენით ცვლადი სადაც შეინახავთ სიტყვა "hidroeleqtrodadguri" ,თქვენი დავალება 
+# იქნება რომ გამოიტანოთ მხოლოდ "hodroele" გამოიყენეთ სლაისინგი
+var = "hidroeleqtrodadguri"
+print(var[0:8])
