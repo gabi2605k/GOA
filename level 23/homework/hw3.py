bb817528e5574827564ea91cd4d30207b158928c
gabi2605k <gabriel.qarqashadze@icloud.com>
2025-06-25 20:55:05 +0400

    commit

---
+++ level 23/homework/hw3.py
@@ -0,0 +1,9 @@
+# 3)შექმენი ცვლადი და შეინახე რაიმე სტრინგი,შენი დავალებაა WHILE FOR/LOOP გამოყენებით
+#  გამოიტანო თითეული ასო ამ სტრინგიდან
+name = "gabo"
+s = 0
+while s < len(name):
+    print(name[s])
+    s += 1
+for i in name:
+    print(i)
---
+++ level 23/homework/hw5.py
@@ -0,0 +1,8 @@
+# 5)მომხმარებელს 10 ჯერ შემოაყვანინე რიცხვი for loop/while loop გამოყენებით  
+s = 0
+while s != 10:
+    n = int(input("Enter num:"))
+    s += 1
+
+for i in range(0,10):
+    num = int(input("Enter num:"))
